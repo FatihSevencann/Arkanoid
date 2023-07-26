3a8943082b40c4c471d931312fc283e1e6b0e379
Fatih Sevencan <fatihsevencan.dev@gmail.com>
2023-07-27 00:04:21 +0300

    feat(ui): #Created SplashScene and added UI elements

---
+++ arkanoid/Classes/Scenes/SplashScene.cpp
@@ -0,0 +1,41 @@
+#include "SplashScene.h"
+
+USING_NS_CC;
+
+Scene* SplashScene::createScene()
+{
+    auto scene=Scene::create();
+    auto layer=SplashScene::create();
+    scene->addChild(layer);
+    return scene;
+}
+
+bool SplashScene::init()
+{
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+    createBackground();
+
+    return true;
+}
+
+void SplashScene::createBackground()
+{
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+    auto sprite=Sprite::create("splashBackground.png");
+
+    sprite->setPosition(Vec2(visibleSize.width/2 +origin.x,visibleSize.height/2+origin.y));
+
+    float scaleX = visibleSize.width / sprite->getContentSize().width;
+    float scaleY = visibleSize.height / sprite->getContentSize().height;
+
+    sprite->setScale(scaleX, scaleY);
+    this->addChild(sprite);
+}
