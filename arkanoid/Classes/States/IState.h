d41d5168180300170bd61768efada9e709b9af00
Fatih Sevencan <fatihsevencan.dev@gmail.com>
2023-08-06 19:05:30 +0300

    feat(state): Integrate EventManager into IState

--- arkanoid/Classes/States/IState.h
+++ arkanoid/Classes/States/IState.h
@@ -1,30 +1,37 @@
 #pragma once
+
 #include <memory>
+#include <utility>
+#include "Base/System/Events/EventManager.h"
 
 class StateMachine;
 
 class IState {
 public:
-    explicit IState(std::shared_ptr<StateMachine> stateHolder) : states(stateHolder) {}
+    explicit IState(std::shared_ptr<StateMachine> stateHolder, EventManager &eventManager) : states(
+            std::move(stateHolder)), mEventManager(&eventManager) {}
 
-    void setEnter()
-    {
+    void setEnter() {
         enter();
     }
-    void setExecute()
-    {
+
+    void setExecute() {
         execute();
     }
-    void setExit()
-    {
+
+    void setExit() {
         exit();
     }
+
 protected:
     std::shared_ptr<StateMachine> states;
+    EventManager *mEventManager;
 private:
 
     virtual void enter() = 0;
+
     virtual void execute() = 0;
+
     virtual void exit() = 0;
 
 };
