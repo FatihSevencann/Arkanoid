db14341d6456ce050ecd93447b287af8b31b9071
Fatih Sevencan <fatihsevencan.dev@gmail.com>
2023-08-06 19:45:55 +0300

    feat(scene): Integrate EventManager and event notifications in GameScene

--- arkanoid/Classes/Scenes/GameScene.cpp
+++ arkanoid/Classes/Scenes/GameScene.cpp
@@ -1,23 +1,46 @@
 #include "GameScene.h"
 #include "ui/UIButton.h"
+#include <memory>
 USING_NS_CC;
 
 using namespace cocos2d::ui;
 
-Scene* GameScene::createScene()
+Scene* GameScene::createScene(EventManager* eventManager)
 {
     auto scene=Scene::create();
-    auto layer=GameScene::create();
+    auto layer=GameScene::create(eventManager);
     scene->addChild(layer);
     return scene;
 }
-bool GameScene::init() {
+
+GameScene* GameScene::create(EventManager* eventManager)
+{
+    std::unique_ptr<GameScene> layer(new GameScene());
+    if (layer && layer->init(eventManager))
+    {
+        return layer.release();
+    }
+    else
+    {
+        return nullptr;
+    }
+}
+
+bool GameScene::init(EventManager* eventManager)
+{
     if (!Layer::init()) {
         return false;
     }
+    mEventManager = eventManager;
     auto visibleSize = Director::getInstance()->getVisibleSize();
     createGameLabel(visibleSize);
     createGameBackground(visibleSize);
+    createBackButton(visibleSize);
+
+
+
+    return true;
+}
 }
 
 void GameScene::createGameLabel(Size visibleSize )
@@ -36,4 +59,17 @@ void GameScene::createGameBackground(Size visibleSize)
     sprite->setScale(scaleX, scaleY);
     this->addChild(sprite);
 }
+void GameScene::createBackButton(Size visibleSize)
+{
+    auto backButton = Button::create("CloseNormal.png", "");
+    backButton->setPosition(Vec2(visibleSize.width*1.7f, visibleSize.height*0.95f));
+    backButton->setTag(3);
+    this->addChild(backButton);
 
+    backButton->addTouchEventListener([=](Ref* sender, Widget::TouchEventType type) {
+        if (type == Widget::TouchEventType::BEGAN) {
+
+            mEventManager->notifyEvent(Events::GAME_BACK);
+        }
+    });
+}
