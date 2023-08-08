ad035dd6a6f411d9e4f102a61d146be5fcdfcc76
Fatih Sevencan <fatihsevencan.dev@gmail.com>
2023-08-08 21:14:42 +0300

    feat(paddle): #Override functions from MovableObject

--- arkanoid/Classes/Base/Entity /Paddle.h
+++ arkanoid/Classes/Base/Entity /Paddle.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include "cocos2d.h"
 #include "Base/Component/RenderableObject.h"
 #include "Ball.h"
@@ -7,7 +6,31 @@
 class Paddle : public MovableObject, public IObserver{
 public:
     Paddle(const cocos2d::Vec2 Vec2, const std::string& spriteFile, float speed);
+
     ~Paddle();
+
+    float getWidth() const;
+    float getHeight() const;
+
+    float getLeft() const override;
+    float getRight() const override;
+    float getTop() const override;
+    float getBottom() const override;
+
+    void moveLeft();
+    void moveRight();
+    void stayAtPlace();
+
+
+    void setSize(const cocos2d::Size& size);
+    cocos2d::Size getSize() const;
+
+    void update(float delta) override;
+
+private:
+    virtual bool init() override;
+    cocos2d::Size paddleSize;
+
 
 };
 
