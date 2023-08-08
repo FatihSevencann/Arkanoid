7668add90a987f2fa3dd3d0bbe20fecbe3cd57a8
Fatih Sevencan <fatihsevencan.dev@gmail.com>
2023-08-08 20:09:34 +0300

    feat(ball): Implement Ball behavior and interactions
    # Overridden functions in Ball class: getSize, setSize, getRadius, setRadius, getLeft, getRight, getTop, getBottom.
    #Added IObservable interface to Ball class for observer support.
    #Implemented attach, detach, and notifyObservers methods in Ball class for observer management.
    #Ball now updates observers of position changes during its update.
    #These changes enable Ball objects to efficiently handle collisions and interactions with other objects in the game.

--- arkanoid/Classes/Base/Entity /Ball.cpp
+++ arkanoid/Classes/Base/Entity /Ball.cpp
@@ -4,9 +4,69 @@
 Ball::Ball( cocos2d::Vec2 vec2, const std::string &spriteFile, float ball_velocity)
         :
         MovableObject(vec2, cocos2d::Sprite::create(spriteFile), ball_velocity,
-                      cocos2d::Vec2(ball_velocity, ball_velocity))
+                      cocos2d::Vec2(ball_velocity, ball_velocity)), mBallSize(cocos2d::Size(0, 0))
 {
-
+    mBallSize = dynamic_cast<cocos2d::Sprite*>(sprite)->getContentSize();
 
 }
 
+Ball::~Ball() noexcept
+{
+
+}
+cocos2d::Size Ball::getSize() const
+{
+    return mBallSize;
+}
+
+void Ball::setSize(const cocos2d::Size& size)
+{
+    mBallSize = size;
+    dynamic_cast<cocos2d::Sprite*>(sprite)->setScaleX(size.width / sprite->getContentSize().width);
+    dynamic_cast<cocos2d::Sprite*>(sprite)->setScaleY(size.height / sprite->getContentSize().height);
+}
+float Ball::getRadius() const
+{
+    return getContentSize().width * getScaleX() * 0.5f;
+}
+
+void Ball::setRadius(float radius)
+{
+    setScale(radius * 2 / getContentSize().width);
+}
+float Ball::getLeft() const
+{
+    return getPosition().x - getRadius();
+}
+
+float Ball::getRight() const {
+    return getPosition().x + getRadius();
+}
+
+float Ball::getTop() const {
+    return getPosition().y - getRadius();
+}
+
+float Ball::getBottom() const {
+    return getPosition().y + getRadius();
+}
+
+
+void Ball::attach(IObserver *observer) {
+    mObservers.push_back(observer);
+}
+
+void Ball::detach(IObserver *observer) {
+    mObservers.remove(observer);
+}
+
+void Ball::notifyObservers() {
+    for(IObserver *iter : mObservers) {
+        iter->handleBallPosChange(this);
+    }
+}
+
+void Ball::update(float dt) {
+    MovableObject::update(dt);
+    notifyObservers();
+}
