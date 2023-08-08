03aac8b7a530847604091e1300ce9ee9db77cc98
Fatih Sevencan <fatihsevencan.dev@gmail.com>
2023-08-08 20:00:07 +0300

    feat(ball): Create BallObserver for interactive behavior with paddle and bricks
    #The BallObserver class implements the IObserver interface to be notified of changes in Ball's position.he Ball class is updated to include the IObservable interface to support observers.This change sets the foundation for handling collisions and interactions between the Ball, Paddle, and Bricks in the game.

--- arkanoid/Classes/Base/Entity /Ball.cpp
+++ arkanoid/Classes/Base/Entity /Ball.cpp
@@ -1,5 +1,12 @@
-//
-// Created by Fatih Sevencan on 8.08.2023.
-//
-
 #include "Ball.h"
+#include <list>
+
+Ball::Ball( cocos2d::Vec2 vec2, const std::string &spriteFile, float ball_velocity)
+        :
+        MovableObject(vec2, cocos2d::Sprite::create(spriteFile), ball_velocity,
+                      cocos2d::Vec2(ball_velocity, ball_velocity))
+{
+
+
+}
+
--- arkanoid/Classes/Base/Entity /Ball.h
+++ arkanoid/Classes/Base/Entity /Ball.h
@@ -1,15 +1,27 @@
 #pragma once
 
 #include "cocos2d.h"
-#include "Base/Component/RenderableObject.h"
 #include <list>
+#include "Base/Component/RenderableObject.h"
 
+class IObserver;
 
-class Ball  {
+class IObservable {
 public:
-
-private:
-
+    virtual void attach(IObserver *observer) = 0;
+    virtual void detach(IObserver *observer) = 0;
 };
 
 
+class Ball : public MovableObject, public IObservable {
+public:
+    Ball( cocos2d::Vec2 vec2, const std::string &spriteFile, float ball_velocity);
+    ~Ball() override;
+
+
+};
+
+class IObserver {
+public:
+    virtual void handleBallPosChange(Ball *ball) = 0;
+};
