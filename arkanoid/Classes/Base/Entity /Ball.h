8eed86100837ca8158266c2e451ad3b53d15dc87
Fatih Sevencan <fatihsevencan.dev@gmail.com>
2023-08-08 20:05:48 +0300

    feat(ball): Implement observer pattern and MovableObject for Ball interactions
    #Override functions from MovableObject and Observer in Ball class.

--- arkanoid/Classes/Base/Entity /Ball.h
+++ arkanoid/Classes/Base/Entity /Ball.h
@@ -18,7 +18,25 @@ public:
     Ball( cocos2d::Vec2 vec2, const std::string &spriteFile, float ball_velocity);
     ~Ball() override;
 
+    float getLeft()const override;
+    float getRight()const override;
+    float getTop()const override;
+    float getBottom()const override;
 
+    float getRadius()const;
+    void setRadius(float radius);
+
+    void setSize(const cocos2d::Size& size);
+    cocos2d::Size getSize() const;
+
+    void attach(IObserver *observer);
+    void detach(IObserver *observer);
+    void update(float dt);
+private:
+    std::list<IObserver*> mObservers;
+    cocos2d::Size mBallSize;
+
+    void notifyObservers();
 };
 
 class IObserver {
