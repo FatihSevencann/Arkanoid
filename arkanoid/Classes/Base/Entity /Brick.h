cdd2347f95930837a1c0df1c0456a2f22b0b13f7
Fatih Sevencan <fatihsevencan.dev@gmail.com>
2023-08-09 10:57:46 +0300

    feat(brick): Implement Brick class as a RenderableObject and Observer
    #This commit introduces the Brick class, which inherits from RenderableObject and implements the IObserver interface to handle interactions with the Ball object.

--- arkanoid/Classes/Base/Entity /Brick.h
+++ arkanoid/Classes/Base/Entity /Brick.h
@@ -1,5 +1,22 @@
 #pragma once
-class Brick{
+#include "Ball.h"
+#include "Base/Component/RenderableObject.h"
+class Brick: public RenderableObject,public IObserver{
+    Brick(const cocos2d::Vec2 Vec2,const std::string& spriteFile,int durability);
+    ~Brick();
 
+    float getWidth()const;
+    float getHeight()const;
+
+    void setSize(const cocos2d::Size& size);
+    cocos2d::Size getSize() const;
+
+    float getLeft()const override;
+    float getRight()const override;
+    float getTop()const override;
+    float getBottom()const override;
+
+    bool isAlive()const;
+    void handleBallPosChange(Ball *ball) override;
 };
 
