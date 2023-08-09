47ec39a0b3ca16e7562973f9357a0b18f7fb0fe3
Fatih Sevencan <fatihsevencan.dev@gmail.com>
2023-08-09 11:50:36 +0300

    feat(brick): Implement size and position-related functions for Brick class
    #In this commit, the Brick class's size and position-related functions are implemented, including getters and setters for size and methods to retrieve the width, height, right, left, top, and bottom values.

--- arkanoid/Classes/Base/Entity /Brick.cpp
+++ arkanoid/Classes/Base/Entity /Brick.cpp
@@ -7,7 +7,10 @@ Brick::Brick(const cocos2d::Vec2 vec2, const std::string &spriteFile, int durabi
     UpdateBrickTypes();
     mBrickSize=dynamic_cast<cocos2d::Sprite*>(sprite)->getContentSize();
 }
+Brick::~Brick() noexcept
+{
 
+}
 void Brick::UpdateBrickTypes()
 {
     switch (mLifeDuration)
@@ -23,5 +26,42 @@ void Brick::UpdateBrickTypes()
             break;
     }
 }
+cocos2d::Size Brick::getSize() const
+{
+    return mBrickSize;
+}
+float Brick::getWidth() const
+{
+    return mBrickSize.width;
+}
+float Brick::getHeight() const
+{
+    return mBrickSize.height;
+}
+void Brick::setSize(const cocos2d::Size &size)
+{
+    mBrickSize=size;
+    dynamic_cast<cocos2d::Sprite*>(sprite)->setScaleX(size.width / sprite->getContentSize().width);
+    dynamic_cast<cocos2d::Sprite*>(sprite)->setScaleY(size.height / sprite->getContentSize().height);
+}
+float Brick::getRight() const
+{
+    return getPosition().x+mBrickSize.width/2;
+}
+float Brick::getLeft() const
+{
+    return getPosition().x-mBrickSize.width/2;
+}
+float Brick::getTop() const
+{
+    return getPosition().y+mBrickSize.height/2;
+}
+float Brick::getBottom() const
+{
+    return getPosition().y-mBrickSize.height/2;
+}
+
+
+
 
 
